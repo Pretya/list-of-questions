808727853031c914fc22120c64c91c204376be55
Yurii Shulika <jurgen12344@gmail.com>
2023-03-24 16:59:48 +0200

    Update data add button for change status and background style

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const listElement = document.getElementById('list');
+const listCheckbox = document.querySelector('list-checkbox');
 
 // ! Render List ==============
 const renderList = (el, index) => {
@@ -6,7 +8,7 @@ const renderList = (el, index) => {
   const stateSpan = elStateTrue ? 'has-text-success' : 'has-text-danger';
   const stateIcon = elStateTrue ? 'fa-check-square' : 'fa-ban';
   const stateText = elStateTrue ? 'Разобрался' : 'Не разобрался';
-    document.getElementById('list').insertAdjacentHTML('beforeend', 
+  listElement.insertAdjacentHTML('beforeend', 
               `<div class="card p-5 mt-3 has-text-link has-background-white">
                     ${listIndex}. ${el.question}
                       <span class="icon-text ${stateSpan}">
@@ -15,7 +17,11 @@ const renderList = (el, index) => {
                         </span>
                         <span>${stateText}</span>
                       </span>
-                </div>`
+                      <div class="buttons mt-3">
+                        <button class="button is-link is-outlined" data-id="${index}">Изменить статус</button>
+                      </div>
+                </div>
+                `
     );
 }
 
@@ -23,29 +29,24 @@ const renderList = (el, index) => {
 const checkBoxUnderstood = document.getElementById('check-understood');
 const checkBoxDidntUnderstand = document.getElementById('check-didntUnderstand');
 
-const checkboxAllUncheck = () => {
-  if(checkBoxDidntUnderstand.checked == false && checkBoxUnderstood.checked == false) {
-    showAllQuestinon();
-  }
-}
-
-function checkBoxStateTrue() {
-    if(checkBoxUnderstood.checked){
-      checkBoxDidntUnderstand.checked = false;
-    } 
-    if(checkBoxDidntUnderstand.checked){
-      checkBoxUnderstood.checked = false;
-    }
-    checkboxAllUncheck();
-}
-
-
-function checkBoxStateFalse() {
-    if(checkBoxDidntUnderstand.checked){
-      checkBoxUnderstood.checked = false;
-    }
-    checkboxAllUncheck();
-}
+document.querySelectorAll('.input-checkbox')
+  .forEach((el) => {
+    el.addEventListener('change', function(e){
+      let targetCheckbox = e.target.closest('input');
+        if(targetCheckbox === checkBoxUnderstood) {
+          checkBoxFilterTrue();
+          checkBoxDidntUnderstand.checked = false;
+        }
+        else if(targetCheckbox === checkBoxDidntUnderstand) {
+          checkBoxFilterFalse();
+          checkBoxUnderstood.checked = false;
+        } 
+        if (!checkBoxDidntUnderstand.checked && !checkBoxUnderstood.checked){
+          showAllQuestinon();
+        }
+          subscribe ()
+    })
+  })
 
 // ! Textarea ===============
   function submitQueastion() {
@@ -71,19 +72,52 @@ function checkBoxFilterTrue() {
   clearList();
   const questionStatusTrue = list.filter(el => el.state == true);
   questionStatusTrue.forEach(renderList);
+  
 }
 
 function checkBoxFilterFalse() {
   clearList();
   const questionStatusFalse = list.filter(el => el.state == false);
   questionStatusFalse.forEach(renderList);
+  
 }
 
 function showAllQuestinon() {
   clearList();
   list.forEach(renderList);
+  subscribe ()
 }
 showAllQuestinon();
+
+// ! Change status button =============
+function changeStatus(id) {
+ const currentStatus = list[id];
+ currentStatus.state = !currentStatus.state;
+ checkBoxDidntUnderstand.checked = false;
+ checkBoxUnderstood.checked = false;
+ showAllQuestinon();
+}
+function subscribe () {
+  document.querySelectorAll('#list > div .button')
+  .forEach((el,) => {
+      const idButton = el.dataset.id;
+        el.addEventListener('click', function(){
+        changeStatus(idButton);
+        });
+
+        el.addEventListener('mouseover', function(e) {
+          let targetButton = e.target.closest('button');
+          if(!targetButton) return;
+          targetButton.style.cssText = `background-color: #764d9d; transition: all .3s linear;`
+        })
+
+        el.addEventListener('mouseout', function(e) {
+          let targetButton = e.target.closest('button');
+          if(!targetButton) return;
+          targetButton.style.cssText = `transition: all .3s linear;`
+        })
+})
+}
 
 
 
